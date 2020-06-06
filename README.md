9c82ad8960a76d7ecd8d89584021e8d1fa1053eb
ebcms <1540837821@qq.com>
2020-06-06 10:19:31 +0800

    1

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # psr3-delegate
+
 php psr3 delegate
+
+## Installation
+
+``` cmd
+composer require ebcms/psr3-delegate
+```
+
+## Usage
+
+``` php
+$logger = new \Ebcms\Logger\Delegate();
+
+$null_logger= new \Psr\Log\NullLogger();
+$logger->addLogger($null_logger);
+$logger->addLogger(LoggerInterface $...);
+
+$logger->debug('test..');
+```
---
+++ src/Logger/Delegate.php
@@ -0,0 +1,53 @@
+<?php
+namespace Ebcms\Logger;
+
+use Psr\Log\InvalidArgumentException;
+use Psr\Log\LoggerInterface;
+use Psr\Log\LoggerTrait;
+use Psr\Log\LogLevel;
+
+class Delegate implements LoggerInterface
+{
+    use LoggerTrait;
+
+    /**
+     * @var LoggerInterface[] $loggers
+     */
+    protected $loggers = [];
+
+    public function addLogger(LoggerInterface $logger)
+    {
+        $this->loggers[] = $logger;
+    }
+
+    /**
+     * Logs with an arbitrary level.
+     *
+     * @param mixed  $level
+     * @param string $message
+     * @param array  $context
+     *
+     * @return void
+     *
+     * @throws \Psr\Log\InvalidArgumentException
+     */
+    public function log($level, $message, array $context = array())
+    {
+        if (!in_array($level, [
+            LogLevel::EMERGENCY,
+            LogLevel::ALERT,
+            LogLevel::CRITICAL,
+            LogLevel::ERROR,
+            LogLevel::WARNING,
+            LogLevel::NOTICE,
+            LogLevel::INFO,
+            LogLevel::DEBUG])
+        ) {
+            throw new InvalidArgumentException('Unsupported log level:' . $level);
+        }
+
+        foreach ($this->loggers as $logger) {
+            $logger->log($level, $message, $context);
+        }
+    }
+}
